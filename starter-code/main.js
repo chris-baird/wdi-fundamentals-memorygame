246f140bd95b4805a18a3a0cce25c0f2e81c2339
Chris Baird <chrisbaird@chriss-mbp.attlocal.net>
2016-10-14 13:48:44 -0700

    added game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,22 @@
 var cardOne = "queen";
 var cardTwo = "queen";
-var carThree = "king";
+var cardThree = "king";
 var cardFour = "king";
+
+if (cardTwo === cardFour) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
+
+if (cardOne === cardTwo) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
+
+if (cardThree === cardFour) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.");
+}
