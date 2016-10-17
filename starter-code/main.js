f4f7f2d48eb73dd0479279e74aada9b2ef1b30cb
Chris Baird <chrisbaird@chriss-mbp.attlocal.net>
2016-10-17 13:53:38 -0700

    changed logic code.

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,18 +5,11 @@ var cardFour = "king";
 
 if (cardTwo === cardFour) {
     alert("You found a match!");
-} else {
-    alert("Sorry, try again.");
 }
-
-if (cardOne === cardTwo) {
-    alert("You found a match!");
-} else {
-    alert("Sorry, try again.");
+else if (cardOne === cardTwo) {
+  alert("You found a match!");
 }
-
-if (cardThree === cardFour) {
-    alert("You found a match!");
-} else {
-    alert("Sorry, try again.");
+else if (cardThree === cardFour) {
+  alert("You found a match!");
 }
+else alert("Try again");
