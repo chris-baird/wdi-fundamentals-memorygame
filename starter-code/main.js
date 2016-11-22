3943bdfce1a7833b87feb81026705e165285d90c
Chris Baird <chrisbaird@chriss-mbp.attlocal.net>
2016-11-22 11:24:23 -0800

    finished game

--- starter-code/main.js
+++ starter-code/main.js
@@ -15,6 +15,10 @@ else if (cardThree === cardFour) {
 }
 else alert("Try again");*/
 
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay =[];
+
 
 var board = document.getElementById('game-board');
 
@@ -25,9 +29,38 @@ function createCards() {
 
     cardElement.className = 'card';
 
+    cardElement.setAttribute('data-card', cards[i]);
+
+    cardElement.addEventListener('click', isTwoCards);
+
     board.appendChild(cardElement);
   }
 
+}
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+console.log(this.getAttribute('data-card'));
+if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.png'>"; // king
+	} else {
+		this.innerHTML = "<img src='queen.png'>"; //queen
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+function isMatch(cards) {
+  // alert winning message
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
 }
 
 createCards();
